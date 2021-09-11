9b25a37015733f35288347d6585504e63d6d61dd
jaev-977 <john-977@hotmail.com>
2021-09-11 10:15:20 -0500

    cargar botones

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -20,27 +20,100 @@ class _HomePageState extends State<HomePage> {
           Expanded(
             flex: 5,
             child: Container(
-              child: Text("Zona resultados"),
+              child: Row(children: [
+                Text("Zona resultados"),
+              ]),
               color: Colors.blue,
-              height: 300,
             ),
           ),
           Expanded(
             flex: 1,
             child: Container(
-              child: Text("Zona ingreso"),
+              child: Row(children: [
+                Text("Zona de ingreso"),
+              ]),
               color: Colors.red,
             ),
           ),
           Expanded(
             flex: 2,
             child: Container(
-              child: Text("Zona botones"),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: _listaBotones()),
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: _listaBotones2()),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: _listaBotones3()),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: _listaBontones4()),
+                ],
+              ),
               color: Colors.green,
             ),
           ),
         ],
       ),
     );
+  }
+
+  _listaBotones() {
+    return [
+      ElevatedButton(onPressed: () {}, child: Text("7")),
+      ElevatedButton(onPressed: () {}, child: Text("8")),
+      ElevatedButton(onPressed: () {}, child: Text("9")),
+      ElevatedButton(onPressed: () {}, child: Text("/")),
+      ElevatedButton(onPressed: () {}, child: Icon(Icons.replay)),
+      ElevatedButton(onPressed: () {}, child: Text("C"))
+    ];
+  }
+
+  _listaBotones2() {
+    return [
+      ElevatedButton(onPressed: () {}, child: Text("4")),
+      ElevatedButton(onPressed: () {}, child: Text("5")),
+      ElevatedButton(onPressed: () {}, child: Text("6")),
+      ElevatedButton(onPressed: () {}, child: Text("x")),
+      ElevatedButton(onPressed: () {}, child: Text("(")),
+      ElevatedButton(onPressed: () {}, child: Text(")"))
+    ];
+  }
+
+  _listaBotones3() {
+    return [
+      ElevatedButton(onPressed: () {}, child: Text("1")),
+      ElevatedButton(onPressed: () {}, child: Text("2")),
+      ElevatedButton(onPressed: () {}, child: Text("3")),
+      ElevatedButton(onPressed: () {}, child: Text("-")),
+      ElevatedButton(onPressed: () {}, child: Text("x²")),
+      ElevatedButton(onPressed: () {}, child: Text("√"))
+    ];
+  }
+
+  _listaBontones4() {
+    return [
+      ElevatedButton(onPressed: () {}, child: Text("0")),
+      ElevatedButton(onPressed: () {}, child: Text(",")),
+      ElevatedButton(onPressed: () {}, child: Text("%")),
+      ElevatedButton(onPressed: () {}, child: Text("+")),
+      ElevatedButton(
+        onPressed: () {},
+        child: Container(
+          child: Row(
+            children: [
+              Text("="),
+            ],
+          ),
+          color: Colors.green,
+        ),
+      ),
+    ];
   }
 }
