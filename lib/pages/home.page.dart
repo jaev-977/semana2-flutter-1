ffeffa032c79d92596d4e524ab22777c8fb48eea
jaev-977 <john-977@hotmail.com>
2021-09-19 20:40:23 -0500

    operaciones basicas

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 import 'package:semanda2flutter1/widget/botones.widget.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +12,81 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String _resultados = '0';
-  String _ingreso = '0';
+  String _ingreso = '';
+  String _error = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora - John Alejandro E."),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                alignment: Alignment.center,
+                child: Text(_resultados,
+                    style: TextStyle(fontSize: 22, color: Colors.white70)),
+                decoration: BoxDecoration(
+                    color: Colors.lightGreen,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                alignment: Alignment.centerRight,
+                child: Column(
+                  children: [
+                    Text(_ingreso,
+                        style: TextStyle(fontSize: 25, color: Colors.white)),
+                  ],
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+          ),
+          Text(_error, style: TextStyle(fontSize: 25, color: Colors.red)),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Column(
+                  children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: _listaBotones()),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: _listaBotones2()),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: _listaBotones3()),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: _listaBontones4()),
+                  ],
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.black26,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   nClik(String text) {
     setState(() {
@@ -19,57 +94,39 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 2,
-            child: Container(
-              child: Row(children: [
-                Text(_resultados),
-              ]),
-              color: Colors.blue,
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              child: Row(children: [
-                Text(_ingreso),
-              ]),
-              color: Colors.red,
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              child: Column(
-                children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: _listaBotones()),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: _listaBotones2()),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: _listaBotones3()),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: _listaBontones4()),
-                ],
-              ),
-              color: Colors.black,
-            ),
-          ),
-        ],
-      ),
-    );
+  borratodo(String text) {
+    setState(() {
+      _resultados = '0';
+      _ingreso = '';
+    });
+  }
+
+  borrauno(String text) {
+    setState(() {
+      if (_ingreso.length >= 1) {
+        _ingreso = _ingreso.substring(0, _ingreso.length - 1);
+      }
+      if (_ingreso.length == 0) {
+        _ingreso = '0';
+      }
+    });
+  }
+
+  calcular(String text) {
+    _error = '';
+    if (text == "=") {
+      Parser p = Parser();
+      Expression exp = p.parse(_ingreso);
+      ContextModel cm = ContextModel();
+
+      setState(() {
+        _resultados = _ingreso;
+        _ingreso = exp.evaluate(EvaluationType.REAL, cm).toString();
+      });
+    }
+    if (_ingreso == 'Infinity') {
+      _error = 'Esta dividiendo por cero';
+    }
   }
 
   _listaBotones() {
@@ -79,44 +136,130 @@ class _HomePageState extends State<HomePage> {
             nClik("7");
           },
           componente: Text("7")),
-      pintarBoton(metodo: () {}, componente: Text("8")),
-      pintarBoton(metodo: () {}, componente: Text("9")),
-      pintarBoton(metodo: () {}, componente: Text("/")),
-      pintarBoton(metodo: () {}, componente: Icon(Icons.replay)),
-      pintarBoton(metodo: () {}, componente: Text("C")),
+      pintarBoton(
+          metodo: () {
+            nClik("8");
+          },
+          componente: Text("8")),
+      pintarBoton(
+          metodo: () {
+            nClik("9");
+          },
+          componente: Text("9")),
+      pintarBoton(
+          metodo: () {
+            nClik("/");
+          },
+          componente: Text("/")),
+      pintarBoton(
+          metodo: () {
+            borrauno("d");
+          },
+          componente: Icon(Icons.replay)),
+      pintarBoton(
+          metodo: () {
+            borratodo("c");
+          },
+          componente: Text("C")),
     ];
   }
 
   _listaBotones2() {
     return [
-      pintarBoton(metodo: () {}, componente: Text("4")),
-      pintarBoton(metodo: () {}, componente: Text("5")),
-      pintarBoton(metodo: () {}, componente: Text("6")),
-      pintarBoton(metodo: () {}, componente: Text("x")),
-      pintarBoton(metodo: () {}, componente: Text("(")),
-      pintarBoton(metodo: () {}, componente: Text(")")),
+      pintarBoton(
+          metodo: () {
+            nClik("4");
+          },
+          componente: Text("4")),
+      pintarBoton(
+          metodo: () {
+            nClik("5");
+          },
+          componente: Text("5")),
+      pintarBoton(
+          metodo: () {
+            nClik("6");
+          },
+          componente: Text("6")),
+      pintarBoton(
+          metodo: () {
+            nClik("*");
+          },
+          componente: Text("x")),
+      pintarBoton(
+          metodo: () {
+            nClik("(");
+          },
+          componente: Text("(")),
+      pintarBoton(
+          metodo: () {
+            nClik(")");
+          },
+          componente: Text(")")),
     ];
   }
 
   _listaBotones3() {
     return [
-      pintarBoton(metodo: () {}, componente: Text("1")),
-      pintarBoton(metodo: () {}, componente: Text("2")),
-      pintarBoton(metodo: () {}, componente: Text("3")),
-      pintarBoton(metodo: () {}, componente: Text("-")),
-      pintarBoton(metodo: () {}, componente: Text("x²")),
-      pintarBoton(metodo: () {}, componente: Text("√")),
+      pintarBoton(
+          metodo: () {
+            nClik("1");
+          },
+          componente: Text("1")),
+      pintarBoton(
+          metodo: () {
+            nClik("2");
+          },
+          componente: Text("2")),
+      pintarBoton(
+          metodo: () {
+            nClik("3");
+          },
+          componente: Text("3")),
+      pintarBoton(
+          metodo: () {
+            nClik("-");
+          },
+          componente: Text("-")),
+      pintarBoton(
+          metodo: () {
+            nClik("p");
+          },
+          componente: Text("x²")),
+      pintarBoton(
+          metodo: () {
+            nClik("r");
+          },
+          componente: Text("√")),
     ];
   }
 
   _listaBontones4() {
     return [
-      pintarBoton(metodo: () {}, componente: Text("0")),
-      pintarBoton(metodo: () {}, componente: Text(",")),
-      pintarBoton(metodo: () {}, componente: Text("%")),
-      pintarBoton(metodo: () {}, componente: Text("+")),
       pintarBoton(
-          metodo: () {},
+          metodo: () {
+            nClik("0");
+          },
+          componente: Text("0")),
+      pintarBoton(
+          metodo: () {
+            nClik(",");
+          },
+          componente: Text(",")),
+      pintarBoton(
+          metodo: () {
+            nClik("%");
+          },
+          componente: Text("%")),
+      pintarBoton(
+          metodo: () {
+            nClik("+");
+          },
+          componente: Text("+")),
+      pintarBoton(
+          metodo: () {
+            calcular("=");
+          },
           componente: Text("="),
           valorFlex: 2,
           colorbuton: Colors.green),
